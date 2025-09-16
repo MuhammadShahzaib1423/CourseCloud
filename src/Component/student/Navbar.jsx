bd5e7a246045692900f607fdc1d410f7d589588c
unknown <muhammadshahzaib12@gmail.comgit config --global user.name MuhammadShahzaib>
2025-09-16 12:57:22 +0500

    Hero Section Added

--- src/Component/student/Navbar.jsx
+++ src/Component/student/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
   return (
   <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
    <img src={assets.logo} alt="" className='w-28 lg:w-32 cursor-pointer'/>
-   {/* Laptop Screen */}
+
    <div className='hidden md:flex items-center gap-5 text-gray-500'>
     <div className='flex items-center gap-5'>
-      <button>Become Educator</button>
-      | <Link to='/my-enrollments' > My Enrollments</Link>
+    { user  && <>  <button>Become Educator</button>
+      | <Link to='/my-enrollments' > My Enrollments</Link> </>}
     </div>
     { user ? <UserButton /> : <button onClick={() => openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full cursor-pointer'> Create Account</button>}
 
@@ -28,11 +28,12 @@ const Navbar = () => {
     {/* Mobile Screen */}
    <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500 '>
 
-    <div>
-      <button>Become Educator</button>
-      <Link to='/my-enrollments'>My Enrollments</Link>
+    <div className=' flex gap-1 items-center sm:gap-2 max-sm:text-xs '>
+       { user && <>  <button>Become Educator</button>
+       | 
+      <Link to='/my-enrollments'>My Enrollments</Link> </>}
     </div>
-    <button><img src={assets.user_icon} alt="" /></button>
+    {user ? <UserButton /> : <button onClick={() => openSignIn()} > <img src={assets.user_icon} alt="" /></button>}
    </div>
     </div>
   )
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../../Component/student/Hero'
 
 const Home = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className='flex  flex-col items-center space-y-7 text-denter'>
+  <Hero />  
     </div>
   )
 }
